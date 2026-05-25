b56a79bff91ca33dd1f4ce8ded91fad4ec1294be
Indra Ganiarto <indra.gto02@gmail.com>
2026-05-25 23:51:35 +0700

    move index.php on root project

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-define('ROOT_PATH', dirname(__DIR__));
+define('ROOT_PATH', __DIR__);
 define('APP_PATH', ROOT_PATH . '/app');
-define('PUBLIC_PATH', __DIR__);
+define('PUBLIC_PATH', ROOT_PATH . '/public');
 define('CONFIG_PATH', ROOT_PATH . '/config');
 
 date_default_timezone_set('Asia/Jakarta');
